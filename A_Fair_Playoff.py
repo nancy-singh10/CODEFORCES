aceac22ff36d0f51a7daf9cff346d5a730f3a2c9
NANCY <nancysingh0526@gmail.com>
2024-02-27 10:12:30 +0530

    c

--- A_Fair_Playoff.py
+++ A_Fair_Playoff.py
@@ -4,7 +4,7 @@ def solve(l):
     max1 = max(l[0], l[1])
     min1 = min(l[0], l[1])
     max2 = max(l[2], l[3])
-    min2 = min(l[2], l[3])
+    min2 = min(l[2], l[3]
     # print(max1,min1,max2,min2)
     
     if max1 < min2:
